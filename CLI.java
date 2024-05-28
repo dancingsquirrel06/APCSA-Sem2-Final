c4448ef71b6bd3da874df52d3e1cbdf036fe13d4
dancingsquirrel06@gmail.com <dancingsquirrel06@gmail.com>
2024-05-27 19:34:05 -0700

    a

--- CLI.java
+++ CLI.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class EmployeeManagementSystemCLI {
+public class CLI {
     private static List<Employee> employees = new ArrayList<>();
 
     public static void main(String[] args) {
